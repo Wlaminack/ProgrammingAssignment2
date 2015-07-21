95dca00a331cbc5386cb4b0d0702d2ef4f8d10a6
Wlaminack <william@laminack.com>
2015-07-20 21:46:00 -0400

    test in R
    commented and tested in R

--- cachematrix.R
+++ cachematrix.R
@@ -3,33 +3,48 @@
 
 ## Write a short comment describing this function
 
+## sets up a list of functions that can have pull the cached matrix
+## and inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-m <- NULL
+        ##sets up the cached invered matrix
+        inversematrix <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                inversematrix <<- NULL
         }
+        ## allows you to retrieve the original matrix 
         get <- function() x
-        setsolve <- function(solve) m <<- solve
-        getsolve <- function() m
+        ## pass in the solved matrix and then put that matix into the cache
+        setsolve <- function(solve) inversematrix <<- solve
+        ## allows you to retrieve the solved invese matrix
+        getsolve <- function() inversematrix
+        ## returns the list with all the functions in it 
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Checks to see if there is a solved matrix caches in the new list
+## if that is not the case we solve the matrix and put into the cashed
+## value m. if the matrix is already chached it lets you know that
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getsolve()
-        if(!is.null(m)) {
+        inversematrix <- x$getsolve()
+        ## If the inverse martix is cached pull it up
+        
+        if(!is.null(inversematrix)) {
                 message("getting cached data")
-                return(m)
+                return(inversematrix)
         }
+        ## otherwise pull the original matrix
         data <- x$get()
-        m <- solve(data, ...)
-        x$setmean(m)
-        m
+        ## solve it
+        inversematrix <- solve(data, ...)
+        ## put it into cache
+        x$setsolve(inversematrix)
+        ## spit it out
+        inversematrix
 }
-}
+
